67cc581e2895b9bee031b64c21878b9ec083391e
msolorio <msolorio.github@gmail.com>
2021-02-27 22:16:58 -0800

    Adds post show page

--- src/pages/PostShowPage.js
+++ src/pages/PostShowPage.js
@@ -1,11 +1,39 @@
-function PostShowPage(props) {
-  console.log('props in post show page')
-  console.log(props)
-  const postId = props.match.params.postId;
+import React from 'react';
 
-  return (
-    <h1>Post Show Page: {postId}</h1>
-  )
+class PostShowPage extends React.Component {
+  state = {
+    post: null
+  }
+
+  async componentDidMount() {
+    const response = await fetch(`http://localhost:4000/posts/${this.props.match.params.postId}`);
+    const data = await response.json();
+
+    this.setState({ post: data });
+  }
+
+  renderPostInfo() {
+    if (this.state.post) {
+      return (
+        <>
+          <p>{this.state.post.city.name}</p>
+          <h2>{this.state.post.title}</h2>
+          <p>{this.state.post.body}</p>
+        </>
+      );
+    } else {
+      <p>Post Not found</p>
+    };
+  }
+
+  render() {
+    return (
+      <main>
+        <h1>Post Show Page</h1>
+        {this.renderPostInfo()}
+      </main>
+    );
+  }
 }
 
 export default PostShowPage;
